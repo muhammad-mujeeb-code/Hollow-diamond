8396d587c97f3a2ab546b2cd6aa75ffbda4e3402
muhammad-mujeeb-code <muhammadmujeebnarejo@gmail.com>
2026-01-14 22:29:01 +0500

    Update Diamond.cpp
    removed comments

--- Diamond.cpp
+++ Diamond.cpp
@@ -1,35 +1,3 @@
-// #include <iostream>
-// using namespace std;
-// int main(){
-//     int n=11, m;
-
-//     for (int i = 0; i < n; i++)
-//     {
-//         for (int j = 0; j < n-1-i; j++)
-//         {
-//             cout<<" ";
-//         }
-//         for (int k = 0; k <= i ; k++)
-//         {
-//             cout<<"* ";
-//         }
-//         cout<<endl;
-//     }
-//     for (int i = 0; i < n; i++)
-//     {
-//         for (int j = 0; j < i ; j++)
-//         {
-//             cout<<" ";
-//         }
-//         for (int k = 0; k < n-i; k++)
-//         {
-//             cout<<"* ";
-//         }
-//         cout<<endl;
-//     }
-    
-//     return 0;
-// }
 #include <iostream>
 using namespace std;
 int main(){
@@ -98,4 +66,5 @@ int main(){
     cout<<"* "<<endl;
 
     return 0;
+
 }
